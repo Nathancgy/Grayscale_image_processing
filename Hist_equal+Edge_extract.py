30aa62f922016813efb1adc272efad0d071550bf
Nathancgy <guangyu.chen40730-biph@basischina.com>
2023-09-27 14:46:52 +0800

    updated main py

---
+++ Hist_equal+Edge_extract.py
@@ -0,0 +1,119 @@
+import numpy as np
+import os
+import cv2
+import matplotlib.pyplot as plt
+
+
+# defining the canny detector function
+
+# here weak_th and strong_th are thresholds for
+# double thresholding step
+def Canny_detector(img, weak_th = None, strong_th = None):
+	
+	# conversion of image to grayscale
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	
+	# Noise reduction step
+	img = cv2.GaussianBlur(img, (5, 5), 1.4)
+	darkened_img = img * 0.8  
+	darkened_img = np.clip(darkened_img, 0, 255).astype(np.uint8)
+	img = cv2.equalizeHist(darkened_img)
+	# Calculating the gradients
+	gx = cv2.Sobel(np.float32(img), cv2.CV_64F, 1, 0, 3)
+	gy = cv2.Sobel(np.float32(img), cv2.CV_64F, 0, 1, 3)
+	
+	# Conversion of Cartesian coordinates to polar
+	mag, ang = cv2.cartToPolar(gx, gy, angleInDegrees = True)
+	
+	# setting the minimum and maximum thresholds
+	# for double thresholding
+	mag_max = np.max(mag)
+	if not weak_th:weak_th = mag_max * 0.1
+	if not strong_th:strong_th = mag_max * 0.5
+	
+	# getting the dimensions of the input image
+	height, width = img.shape
+	
+	# Looping through every pixel of the grayscale
+	# image
+	for i_x in range(width):
+		for i_y in range(height):
+			
+			grad_ang = ang[i_y, i_x]
+			grad_ang = abs(grad_ang-180) if abs(grad_ang)>180 else abs(grad_ang)
+			
+			# selecting the neighbours of the target pixel
+			# according to the gradient direction
+			# In the x axis direction
+			if grad_ang<= 22.5:
+				neighb_1_x, neighb_1_y = i_x-1, i_y
+				neighb_2_x, neighb_2_y = i_x + 1, i_y
+			
+			# top right (diagonal-1) direction
+			elif grad_ang>22.5 and grad_ang<=(22.5 + 45):
+				neighb_1_x, neighb_1_y = i_x-1, i_y-1
+				neighb_2_x, neighb_2_y = i_x + 1, i_y + 1
+			
+			# In y-axis direction
+			elif grad_ang>(22.5 + 45) and grad_ang<=(22.5 + 90):
+				neighb_1_x, neighb_1_y = i_x, i_y-1
+				neighb_2_x, neighb_2_y = i_x, i_y + 1
+			
+			# top left (diagonal-2) direction
+			elif grad_ang>(22.5 + 90) and grad_ang<=(22.5 + 135):
+				neighb_1_x, neighb_1_y = i_x-1, i_y + 1
+				neighb_2_x, neighb_2_y = i_x + 1, i_y-1
+			
+			# Now it restarts the cycle
+			elif grad_ang>(22.5 + 135) and grad_ang<=(22.5 + 180):
+				neighb_1_x, neighb_1_y = i_x-1, i_y
+				neighb_2_x, neighb_2_y = i_x + 1, i_y
+			
+			# Non-maximum suppression step
+			if width>neighb_1_x>= 0 and height>neighb_1_y>= 0:
+				if mag[i_y, i_x]<mag[neighb_1_y, neighb_1_x]:
+					mag[i_y, i_x]= 0
+					continue
+
+			if width>neighb_2_x>= 0 and height>neighb_2_y>= 0:
+				if mag[i_y, i_x]<mag[neighb_2_y, neighb_2_x]:
+					mag[i_y, i_x]= 0
+
+	weak_ids = np.zeros_like(img)
+	strong_ids = np.zeros_like(img)			
+	ids = np.zeros_like(img)
+	
+	# double thresholding step
+	for i_x in range(width):
+		for i_y in range(height):
+			
+			grad_mag = mag[i_y, i_x]
+			
+			if grad_mag<weak_th:
+				mag[i_y, i_x]= 0
+			elif strong_th>grad_mag>= weak_th:
+				ids[i_y, i_x]= 1
+			else:
+				ids[i_y, i_x]= 2
+	
+	
+	# finally returning the magnitude of
+	# gradients of edges
+	return mag
+
+# Modify your path!
+file_path=''
+frame = cv2.imread(file_path)
+canny_img = Canny_detector(frame)
+min_val = np.min(canny_img)
+max_val = np.max(canny_img)
+
+# Used to add contrast to the extracted edges.
+canny_img_contrast = cv2.normalize(canny_img, None, 0, 255, cv2.NORM_MINMAX)
+canny_img_contrast = cv2.convertScaleAbs(canny_img_contrast)
+frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+res = np.hstack((frame_gray, canny_img))
+
+# Modify your path!
+result_path=''
+cv2.imwrite(result_path, res)
--- Individual_algorithms/Histogram_Equalization.py
+++ Individual_algorithms/Histogram_Equalization.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as plt
 img = cv.imread('', cv.IMREAD_GRAYSCALE)
 
 # Decrease the grayscale values by reducing the pixel intensities
-darkened_img = img * 0.8  # Adjust the multiplication factor (0.6 in this case)
+darkened_img = img * 0.8  # Adjust the multiplication factor (0.8 in this case)
 
 # Keep pixel intensities within the valid range of 0 to 255
 darkened_img = np.clip(darkened_img, 0, 255).astype(np.uint8)
